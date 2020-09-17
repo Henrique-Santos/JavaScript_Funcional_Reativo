95a225f116d877a3e1aa618cee77012049589109
Henrique Santos <henrique.144@outlook.com>
2020-09-17 15:16:23 -0300

    Inciciando estudos/testes avançados de JS

---
+++ funcoes/funcao_1.js
@@ -0,0 +1,19 @@
+// Function Declaration
+function bomDia() {
+  console.log('Bom dia!')
+}
+
+bomDia()
+
+// Function Expression
+const boaTarde = function() {
+  console.log('Boa Tarde!')
+}
+
+boaTarde()
+
+function somar(a, b) {
+  return a + b
+}
+
+console.log(somar(5, 9))
