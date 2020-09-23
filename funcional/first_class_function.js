53444b0fe71ca4e26e4120face7738a4c5d3001c
Henrique Santos <henrique.144@outlook.com>
2020-09-23 14:53:28 -0300

    First Class Function

---
+++ funcional/first_class_function.js
@@ -0,0 +1,10 @@
+// First Class Function
+const x = 3
+const y = function(txt) {
+    return `Esse é o texto: ${txt}`
+}
+const z = () => console.log('Zzzzzz!')
+
+console.log(x)
+console.log(y('Olá'))
+z()
