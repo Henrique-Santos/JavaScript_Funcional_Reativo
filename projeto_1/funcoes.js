a1b81178562e466a00e10f652d52ddc1588ecf16
Henrique Santos <henrique.144@outlook.com>
2020-09-29 15:28:59 -0300

    Entendendo Composiçao

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -1,6 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 
+function composicao(...fns) {
+    return function(valor) {
+        return fns.reduce(async (acc, fn) => {
+            if (Promise.resolve(acc) === acc) return fn(await acc)
+            return fn(acc)
+        }, valor)
+    }
+}
+
 function lerDiretorio(caminho) {
     return new Promise((resolve, reject) => {
         try {
@@ -89,6 +98,7 @@ function ordenarPorAtrubNumerico(attr, ordem = 'asc') {
 }
 
 module.exports = {
+    composicao,
     lerDiretorio,
     lerArquivo,
     lerArquivos,
