fd5adf374d8a0bbdfb093e3251f9a47f307c3523
Henrique Santos <henrique.144@outlook.com>
2020-10-12 16:55:54 -0300

    Entendendo mais de Operadores

---
+++ rxjs/operadores_6.js
@@ -0,0 +1,53 @@
+const { from, Observable } = require('rxjs')
+
+function createPipeableOperator(operatorFn) {
+    return function(source) {
+        return new Observable(subscriber => {
+            const sub = operatorFn(subscriber)
+            source.subscribe({
+                next: sub.next,
+                error: sub.error || (e => subscriber.error(e)),
+                complete: sub.complete || (e => subscriber.complete())
+            })
+        })
+    }
+}
+
+function primeiro() {
+    return createPipeableOperator(subscriber => ({
+        next(valor) {
+            subscriber.next(valor)
+            subscriber.complete()
+        }
+    }))
+}
+
+function ultimo() {
+    let ultimo
+    return createPipeableOperator(subscriber => ({
+        next(v) {
+            ultimo = v
+        },
+        complete() {
+            if (ultimo !== undefined) 
+                subscriber.next(ultimo)
+            subscriber.complete()
+        }
+    }))
+}
+
+function nenhum() {
+    return createPipeableOperator(subscriber => ({
+        next(v) {
+            subscriber.complete()
+        }
+    }))
+}
+
+from([1, 2, 3, 4, 5])
+    .pipe(
+        // primeiro(),
+        // nenhum(),
+        ultimo()
+    )
+    .subscribe(console.log)
